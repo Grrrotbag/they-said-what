b81db789b26555a988284504b7f31f02680c949e
Gerard Keane <gerard.keane@dfkeane.com>
2020-11-24 16:20:06 +0000

    Initial commit from codepen.io

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { quote: "", author: "" };
+  }
+  componentDidMount() {
+    this.getRandomQuote();
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  getRandomQuote() {
+    let url = "https://type.fit/api/quotes"; //thanks to https://github.com/ssokurenko
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        let randomQuote = data[Math.floor(Math.random() * data.length)];
+        this.setState({
+          quote: randomQuote["text"],
+          author: randomQuote["author"],
+        });
+      });
+  }
+
+  handleClick() {
+    this.getRandomQuote();
+  }
+
+  render() {
+    let author = "";
+    if (this.state.author) {
+      author = <h2 id="author">{this.state.author}</h2>;
+    } else {
+      author = "";
+    }
+    return (
+      <div id="body-container">
+        <div id="header-container">
+          <h1 id="header">They said what!?</h1>
+          <p id="subhead">Words of wisdom</p>
+        </div>
+        <div id="quote-box">
+          <div id="quote-container">
+            {author}
+            <h1 id="text">{this.state.quote}</h1>
+          </div>
+          <div id="button-container">
+            <button onClick={this.handleClick.bind(this)} id="new-quote" class="btn">
+              Get new quote <i class="fa fa-refresh"></i>
+            </button>
+            <a href="https://twitter.com/intent/tweet" id="tweet-quote" class="btn">
+              {/* <i class="fa fa-twitter"></i> */}
+              <FontAwesomeIcon icon={faTwitter} />
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
